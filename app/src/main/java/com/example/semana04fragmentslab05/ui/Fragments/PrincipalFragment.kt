d16d0bfeffa89f620320b75ea7632d9e3e2f198e
renzovargas04 <20100038@ue.edu.pe>
2023-09-16 10:37:20 -0500

    Initial commit

---
+++ app/src/main/java/com/example/semana04fragmentslab05/ui/Fragments/PrincipalFragment.kt
@@ -0,0 +1,18 @@
+package com.example.semana04fragmentslab05.ui.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.semana04fragmentslab05.R
+
+class PrincipalFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_principal, container, false)
+    }
+}
